5eec67b9195484be017f5771f1af41c7a2ed77ab
Vlad Imir <ymyr.vlad@gmail.com>
2023-02-04 21:24:55 +0200

    feat: add connect to postgresql

---
+++ web/storage/connect.js
@@ -0,0 +1,21 @@
+const { Sequelize } = require('sequelize');
+const path = require('path');
+const envPath = path.join(__dirname, '..', '.env');
+
+require('dotenv').config({ path: envPath });
+console.log('The number of PORT is:', process.env.DB_PORT);
+
+const DB_PORT = process.env.DB_PORT;
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const DB_NAME = process.env.DB_NAME;
+
+const connect = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: 'postgres'
+});
+
+console.log(connect.options)
+module.exports = connect;
--- web/storage/models/year.js
+++ web/storage/models/year.js
@@ -1,6 +1,8 @@
 
 const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = require('../connect')
+const connect = require('../connect.js')
+console.log(connect.options)
+return;
 
 const Year = sequelize.define('years', {
     // Model attributes are defined here
@@ -9,31 +11,36 @@ const Year = sequelize.define('years', {
       primaryKey: true,
       autoIncrement: true
     },
+
     year: {
       type: DataTypes.INTEGER,
       allowNull: false,
       unique: true 
     },
-    count: {
+
+    count_movies: {
       type: DataTypes.INTEGER,
       defaultValue: 0
     },
-    scan: {
+
+    count_scan_movies: {
       type: DataTypes.INTEGER,
       defaultValue: 0
     },
+
     status: {
       type: DataTypes.INTEGER,
       defaultValue: 0
     },
+
     createdAt: { type: Sequelize.DATE, field: 'created_at', defaultValue: DataTypes.NOW },
     updatedAt: { type: Sequelize.DATE, field: 'updated_at' },
   }, {
     // Other model options go here
   });
 
-  // (async () => {
-  //   await sequelize.sync({ force: true });
-  // })();
+  (async () => {
+    await sequelize.sync({ force: true });
+  })();
 
   module.exports = Year
